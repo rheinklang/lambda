4b1937fd72cef017138c09543fff94a561333613
Jan Biasi <biasijan@gmail.com>
2020-01-27 18:50:54 +0100

    add health-check endpoint

--- src/env.ts
+++ src/env.ts
@@ -7,4 +7,6 @@ export const BUILD_ID = 'undefined';
 export const CONTEXT = 'undefined';
 export const REPOSITORY_URL = 'undefined';
 export const COMMIT_REF = 'undefined';
+export const COCKPIT_URL = 'https://cockpit.rheinklang-festival.ch';
+export const FESTIVAL_SITE_URL = 'https://rheinklang-festival.ch';
 export const BUILD_TIMESTAMP = '2020-01-27T17:42:13.254Z';
---
+++ src/lambda/health-check.ts
@@ -0,0 +1,35 @@
+import { Handler } from "aws-lambda";
+import { LambdaEvent } from "../types/LambdaEvent";
+import { COCKPIT_URL, FESTIVAL_SITE_URL } from '../env';
+import { fetch } from "../native/request";
+
+export const handler: Handler<LambdaEvent> = async () => {
+	let isCockpitUp = false;
+
+	try {
+		await fetch<void>({
+			host: COCKPIT_URL,
+			path: ''
+		});
+		isCockpitUp = true;
+	} catch (err) {
+		console.log(`Health check failed: ${err}`);
+	}
+
+	let isFestivalUp = false;
+
+	try {
+		await fetch<void>({
+			host: FESTIVAL_SITE_URL,
+			path: ''
+		});
+		isFestivalUp = true;
+	} catch (err) {
+		console.log(`Health check failed: ${err}`);
+	}
+
+	return {
+		cockpit: isCockpitUp,
+		festival: isFestivalUp
+	};
+}
